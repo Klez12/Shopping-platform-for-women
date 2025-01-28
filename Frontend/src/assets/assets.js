df71046d1204f8afcec10ef219db33976cb67dcc
Komal <kkumari97531@gmail.com>
2025-01-28 12:40:42 +0530

    initial commit

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,393 @@
+import p_img1_1 from './o.k1_1.png'
+import p_img1_2 from './o.k1_2.png'
+import p_img1_3 from './o.k1_3.png'
+import p_img1_4 from './o.k1_4.png'
+import p_img2_1 from './o.k2_1.png'
+import p_img2_2 from './o.k2_2.png'
+import p_img2_3 from './o.k2_3.png'
+import p_img2_4 from './o.k2_4.png'
+import p_img3_1 from './o.k3_1.png'
+import p_img3_2 from './o.k3_2.png'
+import p_img3_3 from './o.k3_3.png'
+import p_img3_4 from './o.k3_4.png'
+import p_img4_1 from './o.k4_1.png'
+import p_img4_2 from './o.k4_2.png'
+import p_img4_3 from './o.k4_3.png'
+import p_img4_4 from './o.k4_4.png'
+import p_img5_1 from './o.k5_1.png'
+import p_img5_2 from './o.k5_2.png'
+import p_img5_3 from './o.k5_3.png'
+import p_img5_4 from './o.k5_4.png'
+import p_img6_1 from './o.k6_1.png'
+import p_img6_2 from './o.k6_2.png'
+import p_img6_3 from './o.k6_3.png'
+import p_img6_4 from './o.k6_4.png'
+import p_img7_1 from './o.k7_1.png'
+import p_img7_2 from './o.k7_2.png'
+import p_img7_3 from './o.k7_3.png'
+import p_img7_4 from './o.k7_4.png'
+import p_img8_1 from './o.k8_1.png'
+import p_img8_2 from './o.k8_2.png'
+import p_img8_3 from './o.k8_3.png'
+import p_img8_4 from './o.k8_4.png'
+import p_img9_1 from './o.k9_1.png'
+import p_img9_2 from './o.k9_2.png'
+import p_img9_3 from './o.k9_3.png'
+import p_img9_4 from './o.k9_4.png'
+import p_img10_1 from './o.k10_1.png'
+import p_img10_2 from './o.k10_2.png'
+import p_img10_3 from './o.k10_3.png'
+import p_img10_4 from './o.k10_4.png'
+import p_img14 from './o.w1_1.png'
+import p_img15 from './o.w1_2.png'
+import p_img16 from './o.w1_3.png'
+import p_img17 from './o.w1_4.png'
+import p_img18 from './o.w2_1.png'
+import p_img19 from './o.w2_2.png'
+import p_img20 from './o.w2_3.png'
+import p_img21 from './o.w2_4.png'
+import p_img22 from './o.w3_1.png'
+import p_img23 from './o.w3_2.png'
+import p_img24 from './o.w3_3.png'
+import p_img25 from './o.w3_4.png'
+import p_img26 from './o.w4_1.png'
+import p_img27 from './o.w4_2.png'
+import p_img28 from './o.w4_3.png'
+import p_img29 from './o.w4_4.png'
+import p_img30 from './o.w5_1.png'
+import p_img31 from './o.w5_2.png'
+import p_img32 from './o.w6_1.png'
+import p_img33 from './o.w6_2.png'
+import p_img34 from './o.w6_3.png'
+import p_img35 from './o.w6_4.png'
+import p_img36 from './o.w7_1.png'
+import p_img37 from './o.w7_2.png'
+import p_img38 from './o.w7_3.png'
+import p_img39 from './o.w7_4.png'
+import p_img40 from './o.w8_1.png'
+import p_img41 from './o.w8_2.png'
+import p_img42 from './o.w8_3.png'
+import p_img43 from './o.w8_4.png'
+import p_img44 from './o.w9_1.png'
+import p_img45 from './o.w9_2.png'
+import p_img46 from './o.w9_3.png'
+import p_img47 from './o.w9_4.png'
+import p_img48 from './o.w10_1.png'
+import p_img49 from './o.w10_2.png'
+import p_img50 from './o.w10_3.png'
+import p_img51 from './o.w10_4.png'
+import p_img52 from './o.w11_1.png'
+import p_img53 from './o.w11_2.png'
+import p_img54 from './o.w11_3.png'
+import p_img55 from './o.w11_4.png'
+
+
+
+import bin_icon from './bin_icon.png'
+import cart_icon from './cart_icon.png'
+import contact_img from './contact_img.png'
+import cross_icon from './cross_icon.png'
+import dropdown_icon from './dropdown_icon.png'
+import exchange_icon from './exchange_icon.png'
+import logo from './logo.png'
+import menu_icon from './menu_icon.png'
+import hero_img from './hero_img.png'
+import search_icon from './search_icon.png'
+import profile_icon from './profile_icon.png'
+
+
+export const assets ={
+    bin_icon,
+    cart_icon,
+    contact_img,
+    cross_icon,
+    dropdown_icon,
+    exchange_icon,
+    logo,
+    menu_icon,
+    hero_img,
+    search_icon,
+    profile_icon,
+
+}
+
+export const products = [{
+
+    _id : "aaaaa",
+    name: "Girls Pink Checked Top With Shorts",
+    description :"Occasion: Casual Color: Pink Fabric: Cotton Pattern: CheckeredSleeve Length: Sleeveless Neck: Square Neck",
+    price : 529,
+    image : [p_img1_1,p_img1_2,p_img1_3,p_img1_4],
+    category : "Girls",
+    subCategory:" Top and bottom",
+    sizes: ["6-12M","12-18M","18-24M","24-36M"],
+    Date : 87553432,
+    Bestseller:true,
+
+},
+{
+
+    _id : "aaaab",
+    name : "Girls Navy Blue Printed Tiered Top",
+    description :"Occasion: Casual Color: Navy Fabric: Polyster Pattern: Printed Sleeve Length: Sleeveless Neck: Square Neck",
+    price : 455,
+    image :[p_img2_1,p_img2_2,p_img2_3,p_img2_4],
+    category :"Girls",
+    subCategory:" Top and bottom",
+    sizes: ["4Y","5Y"],
+    Date : 67467789,
+    Bestseller: true,
+
+},{
+
+    _id : "aaaac",
+    name:"Girls Jumpsuit",
+    description :"Occasion: Casual Color: multicolour Fabric: Polyster Pattern: Printed Sleeve Length: Sleeveless Neck: Square Neck",
+    price : 455,
+    image :[p_img3_1,p_img3_2,p_img3_3,p_img3_4],
+    category :"Girls",
+    subCategory:" Top and bottom",
+    sizes:  ["4Y","5Y","6Y","7Y","8Y"],
+    Date : 45746447,
+    Bestseller: true,
+
+},{
+
+    _id : "aaaad",
+    name:"Girls Blue Ethnic Motifs Regular Pure Cotton Kurti with Dhoti Pants Dupatta",
+    description :"occasion: Festive Color: White-Blue Fabric: Cotton Pattern: Printed Sleeve Length: Sleeveless Neck: Square Neck Fit: Regular",
+    price :768,
+    image :[p_img4_1,p_img4_2,p_img4_3,p_img4_4],
+    category :"Girls",
+    subCategory:"Top and bottom",
+    sizes: ["4Y","5Y","6Y","7Y","8Y"],
+    Date : 45746440,
+    Bestseller:true,
+
+},{
+
+    _id : "aaaae",
+    name:"Mustard-Multi Printed Sleeveless Polyester Ethenic",
+    description :"occasion: Party Color: Mustard-Multi Fabric: Cotton Pattern: Printed Sleeve Length: Sleeveless Neck: Square Neck Fit: Regular",
+    price :657,
+    image :[p_img5_1,p_img5_2,p_img5_3,p_img5_4],
+    category :"Girls",
+    subCategory:"Top and bottom",
+    sizes: ["4Y","5Y","6Y","7Y","8Y"],
+    Date : 45746444,
+    Bestseller: true,
+
+},{
+
+    _id : "aaaaf",
+    name:"Girls Light-Pink Printed Sleeveless Polyester Ethenic",
+    description :"occasion: Party Color: Pink Fabric: Cotton Pattern: Printed Sleeve Length: Sleeveless Neck: Square Neck Fit: Regular",
+    price :699,
+    image :[p_img6_1,p_img6_2,p_img6_3,p_img6_4],
+    category :"Girls",
+    subCategory:"Top and bottom",
+    sizes: ["4Y","5Y","6Y","7Y","8Y"],
+    Date : 45746494,
+    Bestseller:true,
+
+},{
+
+    _id : "aaaag",
+    name:" Girls DRESS",
+    description :"occasion: Party Color: Blue Fabric: Cotton Pattern: Printed Sleeve Length: Sleeveless Neck: Square Neck Fit: Regular",
+    price :646,
+    image :[p_img7_1,p_img7_2,p_img7_3,p_img7_4],
+    category :"Girls",
+    subCategory:"Top and bottom",
+    sizes: ["4Y","5Y","6Y","7Y","8Y"],
+    Date : 45746494,
+    Bestseller:true,
+
+},{
+
+    _id : "aaaah",
+    name:"Multicoloured Dyed Layered Georgette Dress",
+    description :"occasion: Party Color: Blue Fabric: Cotton Pattern: Printed Sleeve Length: Sleeveless Neck: Square Neck Fit: Regular",
+    price :577,
+    image :[p_img8_1,p_img8_2,p_img8_3,p_img8_4],
+    category :"Girls",
+    subCategory:"Top and bottom",
+    sizes: ["4Y","5Y","6Y","7Y","8Y"],
+    Date : 45746594,
+    Bestseller:true,
+
+},{
+
+    _id : "aaaai",
+    name: "Girls Embellished Net Fit Flare Dress",
+    description :"occasion: Party Color: Navy Fabric: Cotton Pattern: Printed Sleeve Length: Sleeveless Neck: Square Neck Fit: Regular",
+    price : 772,
+    image :[p_img9_1,p_img9_2,p_img9_3,p_img9_4],
+    category :"Girls",
+    subCategory:"Top and bottom",
+    sizes: ["4Y","5Y","6Y","7Y","8Y"],
+    Date : 45746584,
+    Bestseller:true,
+
+},{
+
+    _id : "aaaaj",
+    name: "Girls Floral Embroidered Angrakha Thread Work Kurta With Trousers Dupatta",
+    description :"occasion: Party Color: white Fabric: Cotton Pattern: Printed Sleeve Length: Sleeveless Neck: Square Neck Fit: Regular",
+    price :968,
+    image :[p_img10_1,p_img10_2,p_img10_3,p_img10_4],
+    category :"Girls",
+    subCategory:"Top and bottom",
+    sizes: ["4Y","5Y","6Y","7Y","8Y"],
+    Date : 45746504,
+    Bestseller: true,
+
+},{
+
+    _id : "aaaak",
+    name:"Shirt Collar Regular Straight Kurti",
+    description : "Three-quarter, regular sleeves Straight shape with regular style Knee length with curved hem Knitted regular viscose rayon",
+    price :909,
+    image :[p_img14,p_img15,p_img16,p_img17],
+    category :"Women",
+    subCategory:"Top",
+    sizes: ["S","M","L","XL","XXL","3Xl"],
+    Date : 8736667,
+    Bestseller:false,
+
+},{
+
+    _id : "aaaal",
+    name: "Women Printed Basic Round Neck Jumpsuit",
+    description :"Black and red printed basic jumpsuit ,has a round neck, sleeveless, na pockets",
+    price :2519,
+    image : [p_img18,p_img19,p_img20,p_img21],
+    category :"Women",
+    subCategory:"Top",
+    sizes: ["S","M","L","XL","XXL","3Xl"],
+    Date :  8736667,
+    Bestseller:false,
+
+},{
+
+    _id : "aaaam",
+    name:"Avocado Printed Half Sleeves Shirt and Pyjama Nightsuit Set",
+    description :"This chic set is comfortable as well as feminine, making it a perfect wear for a relaxed evening at home. Club those fluffy room slippers with this one to soak in a chill vibe.",
+    price :999,
+    image :[p_img22,p_img23,p_img24,p_img25],
+    category :"Women",
+    subCategory:"Top and bottom",
+    sizes:["S","M","L","XL","XXL","3Xl"],
+    Date : 7865212,
+    Bestseller: true,
+
+},{
+
+    _id : "aaaan",
+    name:"Women Rust Solid Sherpa Heavy Winter Hooded Woolen Night Suit",
+    description :"This classic night suit will help you set the right tone for any season. Club it up with those fluffy room slippers for a win-win look for leisure hours.",
+    price :1199,
+    image :[p_img26,p_img27,p_img28,p_img29],
+    category :"Women",
+    subCategory:"Top and bottom",
+    sizes: ["S","M","L","XL","XXL","3Xl"],
+    Date :  7865202,
+    Bestseller:true,
+
+},{
+
+    _id : "aaaao",
+    name:"Women Longline Open Front Jacket",
+    description :"Beige solid open front jacket, has a lapel collar, 2 pockets ,has a button closure, long sleeves, straight hemline, polycotton lining",
+    price :4049,
+    image :[p_img30,p_img31],
+    category :"Women",
+    subCategory:"Top ",
+    sizes: ["S","M","L","XL","XXL","3Xl"],
+    Date : 6754382,
+    Bestseller:true,
+},{
+
+    _id : "aaaap",
+    name:"Stand Collar Crop Puffer Jacket",
+    description :"White solid puffer jacket, has a stand collar, 2 pockets ,has a zip closure, long sleeves, curved hemline, polyester lining",
+    price :1019,
+    image :[p_img32,p_img33,p_img34,p_img35],
+    category :"Women",
+    subCategory:"Top ",
+    sizes: ["S","M","L","XL","XXL","3Xl"],
+    Date : 564378,
+    Bestseller: false,
+
+},{
+
+    _id : "aaaaq",
+    name:"Women Maroon Faux Fur Open Front Jacket",
+    description :"Maroon solid Front Open jacket with faux fur trim, has a stand collar, 2 pockets ,has a zip closure, long sleeves, straight hemline, polyester lining",
+    price :1189,
+    image :[p_img36,p_img37,p_img38,p_img39],
+    category :"Women",
+    subCategory:"Top ",
+    sizes: ["S","M","L","XL","XXL","3Xl"],
+    Date : 786544,
+    Bestseller:false,
+
+},{
+
+    _id : "aaaar",
+    name:"Women Maroon Faux Fur Open Front Jacket",
+    description :"Maroon solid Front Open jacket with faux fur trim, has a stand collar, 2 pockets ,has a zip closure, long sleeves, straight hemline, polyester lining",
+    price :1189,
+    image :[p_img40,p_img41,p_img42,p_img43],
+    category :"Women",
+    subCategory:"Top ",
+    sizes: ["S","M","L","XL","XXL","3Xl"],
+    Date : 786545,
+    Bestseller:false,
+
+},{
+
+    _id : "aaaas",
+    name:"Women Navy Blue Solid Lightweight Bomber",
+    description :"Navy Blue solid bomber, has a mock collar, two pockets, zip closure, long sleeves, straight hem, polyester lining",
+    price :759,
+    image :[p_img44,p_img45,p_img46,p_img47],
+    category :"Women",
+    subCategory:"Top ",
+    sizes: ["S","M","L","XL","XXL","3Xl"],
+    Date : 786545,
+    Bestseller:false,
+
+},{
+
+    _id : "aaaat",
+    name: "Women Camel Brown Solid Puffer Jacket",
+    description :"Camel Brown solid jacket, has a mock collar, 2 pockets, zip closure, long sleeves, straight hem, and nylon lining",
+    price :998,
+    image :[p_img48,p_img49,p_img50,p_img51],
+    category :"Women",
+    subCategory:"Top ",
+    sizes: ["S","M","L","XL","XXL","3Xl"],
+    Date : 786545,
+    Bestseller:false,
+
+
+},{
+
+    _id : "aaaau",
+    name:"Women Floral Printed Shoulder Sleeves A-line Midi Dress",
+    description :"Brown floral print A-line dress Shoulder straps Sleeveless, shoulder straps Midi length in straight hem",
+    price :979,
+    image :[p_img52,p_img53,p_img54,p_img55],
+    category :"Women",
+    subCategory:"Top ",
+    sizes: ["S","M","L","XL","XXL","3Xl"],
+    Date : 786545,
+    Bestseller: true,
+
+},
+
+
+]
+
+

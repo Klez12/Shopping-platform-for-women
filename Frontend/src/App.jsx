09b1cc036b290ea95ac14e204d8772a5d87f97ac
komal <kkumari97531@gmail.com>
2025-03-20 05:14:39 +0530

    a commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,7 +10,7 @@ import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
